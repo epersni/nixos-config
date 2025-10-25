e106c0023a23a58fb530b05b40709c94bda652f2
nicklas <nicklas.m.persson@gmail.com>
2025-10-25 10:13:54 +0200

    Move out vim settings

--- home/common/vim.nix
+++ home/common/vim.nix
@@ -1,1 +1,96 @@
+{ config, ... }:
 
+{
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      background = "light";
+      hidden = true;
+      number = true;
+    };
+    extraConfig = ''
+      set colorcolumn=80
+      set ruler
+      set rulerformat=%l\:%c
+      set showmatch
+      set wildmenu
+      set wildmode=longest,list
+      set backspace=2
+      set noswapfile
+      set nobackup
+      set smarttab
+      set shiftwidth=2
+      set tabstop=2
+      set expandtab
+      set autoindent
+      set smartindent
+      set wrap
+      set ttyfast
+      set lazyredraw
+      set visualbell
+      set t_vb=
+      set laststatus=2
+      set mouse=a
+      set ignorecase
+      set hlsearch
+      au FileType gitcommit set cc=72
+      nnoremap <C-j> <C-w>j
+      nnoremap <C-k> <C-w>k
+      nnoremap <C-h> <C-w>h
+      nnoremap <C-l> <C-w>l
+
+      " Rename tabs to show tab number.
+      if exists("+showtabline")
+          function! MyTabLine()
+              let s = ""
+              let wn = ""
+              let t = tabpagenr()
+              let i = 1
+              while i <= tabpagenr("$")
+                  let buflist = tabpagebuflist(i)
+                  let winnr = tabpagewinnr(i)
+                  let s .= "%" . i . "T"
+                  let s .= (i == t ? "%1*" : "%2*")
+                  let s .= " "
+                  let wn = tabpagewinnr(i, "$")
+
+                  let s .= "%#TabNum#"
+                  let s .= i
+                  " let s .= "%*"
+                  let s .= (i == t ? "%#TabLineSel#" : "%#TabLine#")
+                  let bufnr = buflist[winnr - 1]
+                  let file = bufname(bufnr)
+                  let buftype = getbufvar(bufnr, "buftype")
+                  if buftype == "nofile"
+                      if file =~ "/."
+                          let file = substitute(file, ".*\/\ze.", "", "")
+                      endif
+                  else
+                      let file = fnamemodify(file, ":p:t")
+                  endif
+                  if file == ""
+                      let file = "[No Name]"
+                  endif
+                  let s .= " " . file . " "
+                  let i = i + 1
+              endwhile
+              let s .= "%T%#TabLineFill#%="
+              let s .= (tabpagenr("$") > 1 ? "%999XX" : "X")
+              return s
+          endfunction
+          set stal=2
+          set tabline=%!MyTabLine()
+          set showtabline=1
+          highlight link TabNum Special
+      endif
+
+      let g:netrw_sort_direction = "normal"
+      let g:netrw_sort_by = "name"
+      let g:netrw_sort_options = "i"
+      let g:netrw_liststyle = 1
+      let g:netrw_sort_sequence = '[\/]$,\<core\%(\.\d\+\)\=,\.[a-np-z]$,Makefile,makefile,SConstruct,SConscript,*,\.o$,\.obj$,	\.info$,\.swp$,\.bak$,\~$'
+      filetype indent plugin on
+    '';
+  };
+}
--- home/nicklas/home.nix
+++ home/nicklas/home.nix
@@ -3,6 +3,7 @@
 {
   imports = [ 
       ../common/bash.nix
+      ../common/vim.nix
       ../common/git.nix
       ../common/xresources.nix
       ../common/screen.nix
@@ -16,91 +17,6 @@
     extraPackages = hpkgs: [ hpkgs.xmonad-contrib ];
   };
   home.file = {
-    ".vimrc".text = ''
-      set colorcolumn=80
-      set ruler
-      set rulerformat=%l\:%c
-      au FileType gitcommit set cc=72
-      set number
-      set showmatch
-      syntax enable
-      set wildmenu
-      set wildmode=longest,list
-      set hidden
-      set backspace=2
-      set noswapfile
-      set nobackup
-      set smarttab
-      set shiftwidth=2
-      set tabstop=2
-      set expandtab
-      set autoindent
-      set smartindent
-      set wrap
-      nnoremap <C-j> <C-w>j
-      nnoremap <C-k> <C-w>k
-      nnoremap <C-h> <C-w>h
-      nnoremap <C-l> <C-w>l
-      let g:netrw_sort_direction = "normal"
-      let g:netrw_sort_by = "name"
-      let g:netrw_sort_options = "i"
-      let g:netrw_liststyle = 1
-      let g:netrw_sort_sequence = '[\/]$,\<core\%(\.\d\+\)\=,\.[a-np-z]$,Makefile,makefile,SConstruct,SConscript,*,\.o$,\.obj$,	\.info$,\.swp$,\.bak$,\~$'
-      set ttyfast
-      set lazyredraw
-      set visualbell
-      set t_vb=
-      set laststatus=2
-      set mouse=a
-      filetype indent plugin on
-      set ignorecase
-      set hlsearch
-
-      " Rename tabs to show tab number.
-      if exists("+showtabline")
-          function! MyTabLine()
-              let s = ""
-              let wn = ""
-              let t = tabpagenr()
-              let i = 1
-              while i <= tabpagenr("$")
-                  let buflist = tabpagebuflist(i)
-                  let winnr = tabpagewinnr(i)
-                  let s .= "%" . i . "T"
-                  let s .= (i == t ? "%1*" : "%2*")
-                  let s .= " "
-                  let wn = tabpagewinnr(i, "$")
-
-                  let s .= "%#TabNum#"
-                  let s .= i
-                  " let s .= "%*"
-                  let s .= (i == t ? "%#TabLineSel#" : "%#TabLine#")
-                  let bufnr = buflist[winnr - 1]
-                  let file = bufname(bufnr)
-                  let buftype = getbufvar(bufnr, "buftype")
-                  if buftype == "nofile"
-                      if file =~ "/."
-                          let file = substitute(file, ".*\/\ze.", "", "")
-                      endif
-                  else
-                      let file = fnamemodify(file, ":p:t")
-                  endif
-                  if file == ""
-                      let file = "[No Name]"
-                  endif
-                  let s .= " " . file . " "
-                  let i = i + 1
-              endwhile
-              let s .= "%T%#TabLineFill#%="
-              let s .= (tabpagenr("$") > 1 ? "%999XX" : "X")
-              return s
-          endfunction
-          set stal=2
-          set tabline=%!MyTabLine()
-          set showtabline=1
-          highlight link TabNum Special
-      endif
-    '';
     ".xmonad/xmobarrc".text = ''
       Config { font = "-*-Fixed-Bold-R-Normal-*-13-*-*-*-*-*-*-*"
           , borderColor = "black"
