85a6d0c1f61152110c8d355affe4070066ae9fb5
nicklas <nicklas.m.persson@gmail.com>
2025-10-26 09:52:13 +0100

    Add git aliases

--- home/common/git.nix
+++ home/common/git.nix
@@ -6,6 +6,11 @@
     userName = "nicklas";
     userEmail = "nicklas.m.persson@gmail.com";
     extraConfig = {
+      alias = {
+        ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --numstat";
+        lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(reset)%s%C(reset) %C(dim white)- %an%C(reset)'";
+        lb = "for-each-ref --sort=-committerdate refs/heads --format='%(committerdate:relative) %09 %(committerdate:short) %09 %(refname:short)'";
+      };
       core = {
         editor = "vim";
       };
