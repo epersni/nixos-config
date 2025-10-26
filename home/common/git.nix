ffeb1c711aad84800643d49c19601a4c8d10d289
nicklas <nicklas.m.persson@gmail.com>
2025-10-26 10:04:49 +0100

    Change git lg to git hist

--- home/common/git.nix
+++ home/common/git.nix
@@ -8,7 +8,7 @@
     extraConfig = {
       alias = {
         ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --numstat";
-        lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(reset)%s%C(reset) %C(dim white)- %an%C(reset)'";
+        hist = "log --graph --pretty=format:'%C(auto)%h%d %s %C(black)%C(bold)(%cr by %an)' --abbrev-commit --date=relative";
         lb = "for-each-ref --sort=-committerdate refs/heads --format='%(committerdate:relative) %09 %(committerdate:short) %09 %(refname:short)'";
       };
       core = {
